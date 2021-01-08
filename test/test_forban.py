89b7eef4cb5645cdf764f5c7d3a4269e64259f11
Hervé Saint-Amand <git@saintamh.org>
2021-01-08 19:21:23 +0000

    Expand tests

--- test/test_forban.py
+++ test/test_forban.py
@@ -21,37 +21,45 @@ HTML_DOC = ET.HTML(
 
 
 @pytest.mark.parametrize(
-    'collection, test, expected',
+    'collection, test, options, expected',
     [
-        ('abracadabra', r'br.c', 'brac'),
-        ('abracadabra', r'b.a', MoreThanOne),
-        ('abracadabra', r'brr', Mismatch),
+        ('abracadabra', r'br.c', {}, 'brac'),
+        ('abracadabra', r'b.a', {}, MoreThanOne),
+        ('abracadabra', r'b.a', {'allow_many': True}, 'bra'),
+        ('abracadabra', r'brr', {}, Mismatch),
+        ('abracadabra', r'brr', {'allow_mismatch': True}, None),
 
-        (HTML_DOC, 'body/p/b/text()', 'forban'),
-        (HTML_DOC, './body/p/b/text()', 'forban'),
-        (HTML_DOC, '/body/p/b/text()', 'forban'),
-        (HTML_DOC, '//body/p/b/text()', 'forban'),
-        (HTML_DOC, 'b/text()', 'forban'),
-        (HTML_DOC, '//b/text()', 'forban'),
-        (HTML_DOC, 'i/text()', Mismatch),
-        (HTML_DOC, 'p/text()', MoreThanOne),
+        (HTML_DOC, 'body/p/b/text()', {}, 'forban'),
+        (HTML_DOC, './body/p/b/text()', {}, 'forban'),
+        (HTML_DOC, '/body/p/b/text()', {}, 'forban'),
+        (HTML_DOC, '//body/p/b/text()', {}, 'forban'),
+        (HTML_DOC, 'b/text()', {}, 'forban'),
+        (HTML_DOC, '//b/text()', {}, 'forban'),
+        (HTML_DOC, 'i/text()', {}, Mismatch),
+        (HTML_DOC, 'i/text()', {'allow_mismatch': True}, None),
+        (HTML_DOC, 'p/text()', {}, MoreThanOne),
+        (HTML_DOC, 'p/text()', {'allow_many': True}, 'Au large, '),
 
-        (HTML_DOC, 'p b', '<b>forban</b>!'),
-        (HTML_DOC, 'p > b', '<b>forban</b>!'),
-        (HTML_DOC, 'body b', '<b>forban</b>!'),
-        (HTML_DOC, 'body > b', Mismatch),
-        (HTML_DOC, 'p', MoreThanOne),
-        (HTML_DOC, 'p#first b', '<b>forban</b>!'),
-        (HTML_DOC, 'p.second', '<p class="second">Au large, flibustier!</p>'),
+        (HTML_DOC, 'p b', {}, '<b>forban</b>!'),
+        (HTML_DOC, 'p > b', {}, '<b>forban</b>!'),
+        (HTML_DOC, 'body b', {}, '<b>forban</b>!'),
+        (HTML_DOC, 'body > b', {}, Mismatch),
+        (HTML_DOC, 'body > b', {'allow_mismatch': True}, None),
+        (HTML_DOC, 'p', {}, MoreThanOne),
+        (HTML_DOC, 'p', {'allow_many': True}, '<p id="first">Au large, <b>forban</b>!</p>'),
+        (HTML_DOC, 'p#first b', {}, '<b>forban</b>!'),
+        (HTML_DOC, 'p.second', {}, '<p class="second">Au large, flibustier!</p>'),
 
-        (['', None, False, 'boo'], bool, 'boo'),
-        (['', None, False, 'boo'], lambda v: v == 'boom', Mismatch),
-        (['', None, False, 'boo'], lambda v: not v, MoreThanOne),
+        (['', None, False, 'boo'], bool, {}, 'boo'),
+        (['', None, False, 'boo'], lambda v: v == 'boom', {}, Mismatch),
+        (['', None, False, 'boo'], lambda v: v == 'boom', {'allow_mismatch': True}, None),
+        (['', None, False, 'boo'], lambda v: not v, {}, MoreThanOne),
+        (['', None, False, 'boo'], lambda v: not v, {'allow_many': True}, ''),
     ]
 )
-def test_one(collection, test, expected):
+def test_one(collection, test, options, expected):
     try:
-        result = one(test, collection)
+        result = one(test, collection, **options)
     except Exception as ex:  # anything at all, pylint: disable=broad-except
         if isinstance(expected, type) and issubclass(expected, Exception):
             assert isinstance(ex, expected)
