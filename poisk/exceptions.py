509ab54431c6f9087ebf3a11deb4ab303b36d02e
Hervé Saint-Amand <git@saintamh.org>
2021-08-14 17:33:34 +0100

    Better error messages

--- poisk/exceptions.py
+++ poisk/exceptions.py
@@ -4,9 +4,19 @@
 class PoiskException(ValueError):
 
     def __init__(self, needle, haystack):
-        super().__init__(repr(needle))
+        super().__init__(self._compose_message(needle, haystack))
         self.needle = needle
         self.haystack = haystack
+
+    @staticmethod
+    def _compose_message(needle, haystack):
+        message = repr(needle)
+        if isinstance(haystack, (str, list, tuple, set, dict)):
+            haystack_repr = repr(haystack)
+            if len(haystack_repr) > 100:
+                haystack_repr = haystack_repr[:50] + '…' + haystack_repr[50:-1]
+            message += ' in ' + haystack_repr
+        return message
 
 
 class NotFound(PoiskException):
